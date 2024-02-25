c9a98866fc40bbdaf0b862ac30d2dce799321b82
Alexandr Zuev <zuevalex@yandex.ru>
2024-02-25 21:59:07 +0500

    slider 1920px

---
+++ script/slider.js
@@ -0,0 +1,12 @@
+
+var swiper = new Swiper(".services-swiper", {
+  // autoHeight: true,
+  slidesPerView: 3.65,
+  centeredSlides: false,
+  spaceBetween: 156,
+  grabCursor: true,
+  // pagination: {
+  //   el: ".swiper-pagination",
+  //   clickable: true,
+  // },
+});
